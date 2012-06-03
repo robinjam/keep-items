0b30e85248d7b0cebdc9e523efea9d3906e0fce6
James Robinson <admin@robinjam.net>
2012-06-03 02:06:25 +0100

    Remove mid-line carriage returns, general code cleanup

--- src/main/java/net/robinjam/bukkit/keepitems/Death.java
+++ src/main/java/net/robinjam/bukkit/keepitems/Death.java
@@ -13,72 +13,60 @@ import org.bukkit.inventory.ItemStack;
  * @author robinjam
  */
 public class Death {
-
-	private KeepItems plugin;
-	private Location location;
-	private ItemStack[] inventoryContents;
-	private ItemStack[] armorContents;
-	private int experience;
-
+	
+	private final KeepItems plugin;
+	private final Location location;
+	private final ItemStack[] inventoryContents;
+	private final ItemStack[] armorContents;
+	private final int experience;
+	
 	/**
-	 * @param location
-	 *            The location at which the player died
-	 * @param inventoryContents
-	 *            The contents of the player's inventory (a shallow copy of this
-	 *            parameter is automatically made)
-	 * @param armorContents
-	 *            The armour the player was wearing when they died (a shallow
-	 *            copy of this parameter is automatically made)
-	 * @param experience
-	 *            The player's current experience
+	 * @param location The location at which the player died.
+	 * @param inventoryContents The contents of the player's inventory (a shallow copy of this parameter is automatically made).
+	 * @param armorContents The armour the player was wearing when they died (a shallow copy of this parameter is automatically made).
+	 * @param experience The player's current experience.
 	 */
-	public Death(KeepItems plugin, Location location,
-			ItemStack[] inventoryContents, ItemStack[] armorContents,
-			int experience) {
+	public Death(KeepItems plugin, Location location, ItemStack[] inventoryContents, ItemStack[] armorContents, int experience) {
 		this.plugin = plugin;
 		this.location = location;
 		this.inventoryContents = inventoryContents.clone();
 		this.armorContents = armorContents.clone();
 		this.experience = experience;
 	}
-
+	
 	/**
-	 * Drops every item held by this instance, at the location where the player
-	 * died.
+	 * Drops every item held by this instance, at the location where the player died.
 	 */
 	public void drop() {
 		for (ItemStack is : inventoryContents)
 			if (is != null && is.getType() != Material.AIR)
 				location.getWorld().dropItem(location, is);
-
+		
 		for (ItemStack is : armorContents)
 			if (is != null && is.getType() != Material.AIR)
 				location.getWorld().dropItem(location, is);
-
+		
 		if (experience > 0)
-			(location.getWorld().spawn(location, ExperienceOrb.class))
-					.setExperience(experience);
+			(location.getWorld().spawn(location, ExperienceOrb.class)).setExperience(experience);
 	}
-
+	
 	/**
 	 * Gives every item held by this instance to the given player.
 	 * 
-	 * @param player
-	 *            The player to give the items and experience to
+	 * @param player The player to give the items and experience to.
 	 */
 	public void give(final Player player) {
 		player.getInventory().setContents(inventoryContents);
 		player.getInventory().setArmorContents(armorContents);
-
-		// Player#giveExp(int) doesn't work during the respawn event, so
-		// schedule it to run after the player has respawned
+		
+		// Player#giveExp(int) doesn't work during the respawn event, so schedule it to run after the player has respawned
 		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-
+			
 			public void run() {
 				player.giveExp(experience);
 			}
-
+			
 		});
 	}
-
+	
 }
--- src/main/java/net/robinjam/bukkit/keepitems/KeepItems.java
+++ src/main/java/net/robinjam/bukkit/keepitems/KeepItems.java
@@ -21,152 +21,122 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author robinjam
  */
 public class KeepItems extends JavaPlugin implements Listener {
-
+	
 	private Map<Player, Death> deaths = new HashMap<Player, Death>();
-
+	
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 		registerPermissions();
 	}
-
+	
 	@Override
 	public void onDisable() {
-		// When the plugin is disabled, drop all managed items and clear the
-		// list
-		for (Death death : deaths.values()) {
+		for (Death death : deaths.values())
 			death.drop();
-		}
-
+		
 		deaths.clear();
 	}
-
+	
 	/**
-	 * Registers the additional dynamic permissions required by this plugin,
-	 * which cannot be included in the plugin.yml file.
+	 * Registers the additional dynamic permissions required by this plugin, which cannot be included in the plugin.yml file.
 	 */
 	public void registerPermissions() {
 		Map<String, Boolean> children = new HashMap<String, Boolean>();
-
+		
 		// Register keep-items.cause.<type> for each damage cause
 		for (DamageCause cause : DamageCause.values()) {
-			Permission p = new Permission("keep-items.cause."
-					+ cause.name().toLowerCase(),
-					"Allows the user to keep their items and experience when they are killed by "
-							+ cause.name().toLowerCase(),
-					PermissionDefault.FALSE);
+			Permission p = new Permission("keep-items.cause." + cause.name().toLowerCase(), "Allows the user to keep their items and experience when they are killed by " + cause.name().toLowerCase(), PermissionDefault.FALSE);
 			getServer().getPluginManager().addPermission(p);
 			children.put(p.getName(), true);
 		}
-
+		
 		// Register keep-items.cause.*
-		getServer()
-				.getPluginManager()
-				.addPermission(
-						new Permission(
-								"keep-items.cause.*",
-								"Allows the player to keep their items and experience when they die for any reason.",
-								PermissionDefault.TRUE, children));
+		getServer().getPluginManager().addPermission(new Permission("keep-items.cause.*", "Allows the player to keep their items and experience when they die for any reason.", PermissionDefault.TRUE, children));
 	}
-
+	
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onEntityDeath(final EntityDeathEvent event) {
 		// Skip if the entity that died is not a player
 		if (!(event.getEntity() instanceof Player))
 			return;
-
+		
 		Player player = (Player) event.getEntity();
-
+		
 		// If the player already has a death on record, drop the items
 		Death death = deaths.get(player);
 		if (death != null)
 			death.drop();
-
+		
 		// Check if the player has permission for this death cause
-		String damageCause = player.getLastDamageCause().getCause().name()
-				.toLowerCase();
+		String damageCause = player.getLastDamageCause().getCause().name().toLowerCase();
 		if (!player.hasPermission("keep-items.cause." + damageCause)) {
-			System.out
-					.println("Player "
-							+ player.getName()
-							+ " was killed by "
-							+ damageCause
-							+ ", but does not have permission to keep their items. Hint: give them the 'keep-items.cause."
-							+ damageCause
-							+ "' or 'keep-items.cause.*' permission.");
+			System.out.println("Player " + player.getName() + " was killed by " + damageCause + ", but does not have permission to keep their items. Hint: give them the 'keep-items.cause." + damageCause + "' or 'keep-items.cause.*' permission.");
 			return;
 		}
-
+		
 		ItemStack[] inventoryContents = new ItemStack[0];
 		ItemStack[] armorContents = new ItemStack[0];
 		int experience = 0;
-
+		
 		if (player.hasPermission("keep-items.items")) {
 			inventoryContents = player.getInventory().getContents();
 			armorContents = player.getInventory().getArmorContents();
-
+			
 			// Don't drop any items at the death location
 			event.getDrops().clear();
 		}
-
+		
 		if (player.hasPermission("keep-items.experience")) {
 			if (player.hasPermission("keep-items.progress"))
 				experience = calcExperience(player.getLevel(), player.getExp());
 			else
 				experience = calcExperience(player.getLevel());
-
+			
 			// Don't drop any experience at the death location
 			event.setDroppedExp(0);
 		}
-
+		
 		// Register the death event
-		deaths.put(player, new Death(this, player.getLocation(),
-				inventoryContents, armorContents, experience));
+		deaths.put(player, new Death(this, player.getLocation(), inventoryContents, armorContents, experience));
 	}
-
+	
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onPlayerRespawn(final PlayerRespawnEvent event) {
 		Player player = event.getPlayer();
-
-		// If the player has a death on record, drop the items and experience at
-		// their respawn location
+		
+		// If the player has a death on record, drop the items and experience at their respawn location
 		Death death = deaths.remove(player);
 		if (death != null)
 			death.give(player);
 	}
-
+	
 	/**
-	 * Calculates the total amount of experience required to reach the given
-	 * level from level 0.
+	 * Calculates the total amount of experience required to reach the given level from level 0.
 	 * 
-	 * @param level
-	 *            The level for which to calculate the required experience
-	 * @return The amount of experience required
+	 * @param level The level for which to calculate the required experience.
+	 * @return The amount of experience required.
 	 */
 	private int calcExperience(int level) {
 		return calcExperience(level, 0.0f);
 	}
-
+	
 	/**
-	 * Calculates the total amount of experience required to reach the given
-	 * level from level 0.
+	 * Calculates the total amount of experience required to reach the given level from level 0.
 	 * 
-	 * @param level
-	 *            The level for which to calculate the required experience
-	 * @param exp
-	 *            The player's progress towards the next level
-	 * @return The amount of experience required
+	 * @param level The level for which to calculate the required experience.
+	 * @param exp The player's progress towards the next level.
+	 * @return The amount of experience required.
 	 */
 	private int calcExperience(int level, double exp) {
 		int xp = (int) ((7 + (int) Math.floor(level * 3.5)) * exp);
-
+		
 		while (level > 1) {
-			// Calculate the amount of experience required to reach this level
-			// from the previous one
+			// Calculate the amount of experience required to reach this level from the previous one
 			xp += 7 + (int) Math.floor((--level) * 3.5);
 		}
-
+		
 		return xp;
 	}
-
+	
 }
